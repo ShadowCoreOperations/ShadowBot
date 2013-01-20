4cd8297bb009bf69f912a9b39c1cb21deaaf5e84
ShadowCoreOperations <info@shadowcore.eu>
2013-01-20 23:06:22 +0100

    Update shadowbot.py

--- shadowbot.py
+++ shadowbot.py
@@ -1,18 +1,19 @@
 import socket
 import sys
 import string
-import irclib
 
-server = "irc.efnet.org"
-channel = "#channel"
+server = "irc.colosolutions.net"
+port = 6667
+channel = "#shadowcore"
 botnick = "ShadowBot"
-version = "ShadowBot 0.0.1a"
+realname = "ShadowCore info bot 0.0.1"
+ident = "sbot"
 readbuffer=""
 
 irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print "connecting to:"+server
-irc.connect((server, 6667))
-irc.send("USER "+ botnick +" "+ botnick +" "+ botnick +" :ShadowCore IRC Bot 0.0.1\n")
+irc.connect((server, port))
+irc.send("USER "+ ident +" "+ server +" Name: "+realname+"\n")
 irc.send("NICK "+ botnick +"\n")
 irc.send("JOIN "+ channel +"\n")
 
@@ -23,16 +24,3 @@ while 1:
       irc.send('PONG ' + text.split() [1] + '\r\n')
    if text.find('!version') !=-1:
     irc.send("PRIVMSG "+channel+" :+version+ \r\n")
-
-# Handle a CTCP query
-def handleCTCP ( connection, event ):
-
-   # Check to see if it is a "VERSION" query
-   if event.arguments() [ 0 ].upper() == 'VERSION':
-
-      # Reply to the query, giving version information
-      connection.ctcp_reply ( event.source().split ( '!' ) [ 0 ], 'VERSION Python-IRCLib' )
-
-# Create and IRC object and add the handler
-irc = irclib.IRC()
-irc.add_global_handler ( 'ctcp', handleCTCP )
